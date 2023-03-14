ddc9c8a92ec8706545794bc5b415a62d9738fb98
Albert Iost <albertiost@mail.ru>
2023-03-14 22:55:14 +0300

    register function

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .forms import UserRegistrationForm
+from django.views import View
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('dashboard')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+class DashboardView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello world')
