31ad10d5e187f0934183c8aeb4c9c610a090a05d
Albert Iost <albertiost@mail.ru>
2023-03-16 05:37:10 +0300

    change register view from FBV to CBV

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UserRegistrationForm
 from django.views import View
+from django.conf import settings
 # Create your views here.
 
-def register(request):
-    if request.method == 'POST':
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
+        return render(request, 'registration/register.html', {'form': UserRegistrationForm(), 'title': 'Sign up'})
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('dashboard')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
-
+        return render(request, 'registration/register.html', {'form': form, 'title': 'Sign up'})
 
 class DashboardView(View):
     def get(self, request, *args, **kwargs):
