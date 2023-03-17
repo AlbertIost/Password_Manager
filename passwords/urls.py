7ebc8f86fd47e5c54894cd680a5102e899d0866b
Albert Iost <albertiost@mail.ru>
2023-03-17 19:05:23 +0300

    Added redirect from / to /dashboard

--- passwords/urls.py
+++ passwords/urls.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import views as django_auth_views
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 from passwords import views as passwords_views
 
-path('dashboard/', login_required(passwords_views.DashboardView.as_view()), name='dashboard')
+urlpatterns =[
+    path('dashboard/', login_required(passwords_views.DashboardView.as_view()), name='dashboard'),
+    path('', RedirectView.as_view(url='dashboard/'))
+]
