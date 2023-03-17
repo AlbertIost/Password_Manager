a84dd03e9f358585f2e6e4a1d8f59c63419d7ff9
Albert Iost <albertiost@mail.ru>
2023-03-17 19:06:26 +0300

    add path to profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.contrib.auth import views as django_auth_views
 from django.urls import path, include
 from accounts import views as accounts_views
@@ -7,4 +6,5 @@ urlpatterns =[
     path('login/', django_auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', django_auth_views.LogoutView.as_view(), name='logout'),
     path('register/', accounts_views.RegisterView.as_view(), name='register'),
+    path('profile/', accounts_views.ProfileView.as_view(), name='profile')
 ]
