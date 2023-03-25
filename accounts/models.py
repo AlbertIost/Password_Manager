d6a5af48396fa90ca37e26dcb39ce5446584f000
Albert Iost <albertiost@mail.ru>
2023-03-25 17:19:03 +0300

    Action logs model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ActionLogs(models.Model):
+    user = models.OneToOneField(
+        to=User,
+        verbose_name='User',
+        on_delete=models.CASCADE,
+    )
+    action = models.TextField(
+        verbose_name='User action'
+    )
+    note = models.TextField(
+        verbose_name='Note for the action',
+        null=True
+    )
+    danger_level = models.PositiveSmallIntegerField(
+        verbose_name='The level of danger of the action',
+        default=0
+    )
+    execution_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=True
+    )
