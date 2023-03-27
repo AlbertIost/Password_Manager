5f2feefefce76f5c30db399136d320cbdff76830
Albert Iost <albertiost@mail.ru>
2023-03-27 19:17:41 +0300

    writing log in, log out and failed log to action logs was added

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import user_logged_in, user_logged_out, user_login_failed
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
-import passwords.models
-
+from passwords.models import Profile
+from passwords.utils import get_client_ip
 
 class ActionLogs(models.Model):
     profile = models.ForeignKey(
-        to=passwords.models.Profile,
+        to=Profile,
         verbose_name='Profile',
         on_delete=models.CASCADE,
     )
@@ -28,3 +31,34 @@ class ActionLogs(models.Model):
         auto_now_add=True,
         blank=True
     )
+
+
+@receiver(user_logged_in, sender=User)
+def user_logged_in_action_log(sender, request, user, **kwargs):
+    ActionLogs.objects.create(
+        profile=user.profile,
+        action='Logged in',
+        ip_address=get_client_ip(request)
+    )
+
+
+@receiver(user_logged_out, sender=User)
+def user_logged_out_action_log(sender, request, user, **kwargs):
+    ActionLogs.objects.create(
+        profile=user.profile,
+        action='Logged out',
+        ip_address=get_client_ip(request)
+    )
+
+
+@receiver(user_login_failed)
+def user_login_failed_action_log(sender,credentials,request,**kwargs):
+    try:
+        profile = Profile.objects.get(user__username=credentials['username'])
+        ActionLogs.objects.create(
+            profile=profile,
+            action='Failed to log in',
+            ip_address=get_client_ip(request)
+        )
+    except ObjectDoesNotExist:
+        pass
